9a00f3862bb39a4f0abc0b7435466465f4e0b07f
Andreas Remar <andreas.remar@gmail.com>
2012-07-20 11:03:18 +0200

    Use binreloc to locate where we're executing from.

--- src/retro.cc
+++ src/retro.cc
@@ -23,14 +23,75 @@
 #include "Input.h"
 #include "GameLogic.h"
 #include "SoundManager.h"
+#include "binreloc.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 const int BPP = 32;
 const int FS = 0;
 
+bool sanity_check_datadir(char *datadir)
+{
+  char buf[1024];
+  struct stat s;
+
+  sprintf(buf, "%s/../levels/1.lev", datadir);
+
+  if(stat(buf, &s) == 0)
+    {
+      return (s.st_mode & S_IFMT) == S_IFREG;
+    }
+
+  return false;
+}
+
+char *setup_datadir(int argc, char *argv[])
+{
+  BrInitError e;
+  char *datadir = 0;
+  char *alternative = 0;
+
+  if(br_init(&e) == 0)
+    {
+      printf("Failed to init binreloc! Error code: %d\n", e);
+    }
+
+  if(argc > 1)
+    {
+      alternative = argv[1];
+    }
+
+  datadir = br_find_exe_dir(alternative);
+
+  if(!sanity_check_datadir(datadir))
+    {
+      // Try the default location
+      datadir = strdup("/opt/retrobattle");
+
+      if(!sanity_check_datadir(datadir))
+	{
+	  printf("ERROR: Unable to find game data!\n");
+	  printf("       Try providing the path when starting, i.e.:\n");
+	  printf("       # retro <PATH_TO_RETROBATTLE>\n");
+
+	  free(datadir);
+	  datadir = 0;
+	}
+    }
+
+  return datadir;
+}
+
 int main(int argc, char *argv[])
 {
+  char *datadir = setup_datadir(argc, argv);
+
+  if(!datadir)
+    exit(1);
+
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
   Input *input = new Input();
@@ -56,6 +117,7 @@ int main(int argc, char *argv[])
   delete sfx;
   delete gfx;
   delete input;
+  free(datadir);
 
 #ifndef NO_SDL_MIXER
   Mix_CloseAudio();
