6b757bd2ac35e8846d9faa8575ad7d0361feb44f
Andreas Remar <andreas.remar@gmail.com>
2017-12-26 12:01:46 +0100

    Extract image and frame info from animation def

--- data/gfx/ConvertSprite.py
+++ data/gfx/ConvertSprite.py
@@ -5,19 +5,26 @@ def read_file(xmlfile):
     return tree.getroot()
 
 def handle_animation(animation):
-    print(animation.attrib)
     anim = animation.attrib.copy()
+    frames = []
     for child in animation:
         if child.tag == "image":
-            print("Handle image:", child)
+            anim['image'] = handle_image(child)
         elif child.tag == "frame":
-            print("Handle frame:", child)
+            frames.append(handle_frame(child))
+    anim['frames'] = frames
     return anim
+
+def handle_image(image):
+    return image.attrib.copy()
+
+def handle_frame(frame):
+    return frame.attrib.copy()
 
 if __name__ == "__main__":
     root = read_file("good.xml")
     animations = []
     for animation in root:
         animations.append(handle_animation(animation))
-    print(animations)
+    print(animations[0])
 
