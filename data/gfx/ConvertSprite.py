54d8940917ed9adf0fb2674ce1f053ce08fb6a93
Andreas Remar <andreas.remar@gmail.com>
2017-12-26 12:30:02 +0100

    Convert handle_ to read_ and extract read_sprite function

--- data/gfx/ConvertSprite.py
+++ data/gfx/ConvertSprite.py
@@ -4,27 +4,35 @@ def read_file(xmlfile):
     tree = ElementTree.parse('good.xml')
     return tree.getroot()
 
-def handle_animation(animation):
+def read_sprite(sprite):
+    spr = {}
+    animations = []
+    for animation in sprite:
+        animations.append(read_animation(animation))
+    spr['name'] = sprite.attrib['name']
+    spr['animations'] = animations
+    return spr
+
+def read_animation(animation):
     anim = animation.attrib.copy()
     frames = []
     for child in animation:
         if child.tag == "image":
-            anim['image'] = handle_image(child)
+            anim['image'] = read_image(child)
         elif child.tag == "frame":
-            frames.append(handle_frame(child))
+            frames.append(read_frame(child))
     anim['frames'] = frames
     return anim
 
-def handle_image(image):
+def read_image(image):
     return image.attrib.copy()
 
-def handle_frame(frame):
+def read_frame(frame):
     return frame.attrib.copy()
 
 if __name__ == "__main__":
     root = read_file("good.xml")
-    animations = []
-    for animation in root:
-        animations.append(handle_animation(animation))
-    print(animations[0])
-
+    sprite_def = read_sprite(root)
+    print("Sprite name:", sprite_def['name'])
+    for animation in sprite_def['animations']:
+        print("  Animation name:", animation['name'])
